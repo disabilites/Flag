3ee898c9af17bdbea235cbf37e45de3e05c83c0d
horizon <1043922433@qq.com>
2019-05-13 20:49:35 +0800

    Get IPAddr

---
+++ src/main/java/com/springboot/flag/utils/getIpAddr.java
@@ -0,0 +1,35 @@
+package com.springboot.flag.utils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+
+public class getIpAddr {
+
+    public static String getUserIp(HttpServletRequest request){
+        String ip = request.getHeader("x-forwarded-for");
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+            if(ip.equals("127.0.0.1")){
+                InetAddress inet=null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                ip= inet.getHostAddress();
+            }
+        }
+        if(ip != null && ip.length() > 15){
+            if(ip.indexOf(",")>0){
+                ip = ip.substring(0,ip.indexOf(","));
+            }
+        }
+        return ip;
+    }
+}
