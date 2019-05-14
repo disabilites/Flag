02cfc0c6224171694a95d24c0cc2c4a518d8e793
horizon <1043922433@qq.com>
2019-05-14 21:47:21 +0800

    Get UUID

---
+++ src/main/java/com/springboot/flag/utils/UUIDUtils.java
@@ -0,0 +1,9 @@
+package com.springboot.flag.utils;
+
+import java.util.UUID;
+
+public class UUIDUtils {
+    public static String getUUID(){
+        return UUID.randomUUID().toString().replace("-","");
+    }
+}
